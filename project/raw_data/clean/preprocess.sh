6ca586fe6cb0cc22f86cfae10054b2d3d0a1a119
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-05-06 11:21:37 -0500

    Fix preprocess.sh issue when individual data file doesn't contain newline at the end of file

--- project/raw_data/clean/preprocess.sh
+++ project/raw_data/clean/preprocess.sh
@@ -1,13 +1,14 @@
 rm -r tmp
 mkdir tmp
-cat ./selected/*.csv > tmp/raw_dataset.csv
+awk 1 ./selected/*.csv > tmp/raw_dataset.csv
 cat tmp/raw_dataset.csv | uniq > tmp/raw_dataset_uniq.csv
 cut -d ',' -f 1 tmp/raw_dataset_uniq.csv > tmp/X_train.txt
 cut -d ',' -f 2 tmp/raw_dataset_uniq.csv > tmp/y_train.txt
-cat tmp/X_train.txt | gsed 's/[-,]//g' | tr '[:upper:]' '[:lower:]'  > tmp/X_train_lowercase.txt
+cat tmp/X_train.txt | gsed 's/[-, ]//g' > tmp/X_train_2.txt
+# cat tmp/X_train_2.txt | tr '[:upper:]' '[:lower:]'  > tmp/X_train_2.txt
 mkdir tmp/output
-paste -d ',' tmp/X_train_lowercase.txt tmp/y_train.txt > tmp/output/dataset.csv
-echo -e "\"NAME\",\"NATIONALITY\"\n$(cat tmp/output/dataset.csv)" > tmp/output/dataset.csv
+paste -d ',' tmp/X_train_2.txt tmp/y_train.txt > tmp/output/dataset.csv
 echo "number of classes: "
-cut -d ',' -f 2 tmp/output/dataset.csv | uniq | wc -l
+cat tmp/y_train.txt | uniq | wc -l
+echo -e "\"NAME\",\"NATIONALITY\"\n$(cat tmp/output/dataset.csv)" > tmp/output/dataset.csv
 cp tmp/output/dataset.csv ../../data
