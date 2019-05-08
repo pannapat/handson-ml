71892c951e2d1c9666e7381985a9bd91b56bf81c
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-05-08 02:41:32 -0500

    add all the last semester project code

--- project/raw_data/clean/preprocess.sh
+++ project/raw_data/clean/preprocess.sh
@@ -4,7 +4,7 @@ awk 1 ./selected/*.csv > tmp/raw_dataset.csv
 cat tmp/raw_dataset.csv | uniq > tmp/raw_dataset_uniq.csv
 cut -d ',' -f 1 tmp/raw_dataset_uniq.csv > tmp/X_train.txt
 cut -d ',' -f 2 tmp/raw_dataset_uniq.csv > tmp/y_train.txt
-cat tmp/X_train.txt | gsed 's/[-, ]//g' > tmp/X_train_2.txt
+cat tmp/X_train.txt | gsed 's/[-, ]//g' | tr '[:upper:]' '[:lower:]' > tmp/X_train_2.txt
 # cat tmp/X_train_2.txt | tr '[:upper:]' '[:lower:]'  > tmp/X_train_2.txt
 mkdir tmp/output
 paste -d ',' tmp/X_train_2.txt tmp/y_train.txt > tmp/output/dataset.csv
